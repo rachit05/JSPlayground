2c624797dd0f53861783bb83ed6f7b3b6bfd1b93
Rachit shrivastava <rachit.shrivastava70@gmail.com>
2018-06-30 00:28:50 +0530

    this is js play ground

---
+++ index.js
@@ -0,0 +1,20 @@
+function name(n){
+    return n;
+}
+let objects = [
+    {name:'dog',type:'animal'},
+    {name:'cat',type:'animal'},
+    {name:'lotus',type:'flower'},
+    {name:'naman',type:'human'},
+    {name:'audi',type:'car'},
+    {name:'bmw',type:'car'},
+    {name:'john',type:'human'},
+]
+let filterOjects = objects
+                    .filter(function (object){
+                        return object.type == 'animal'
+                    })
+                    .map(function(object){
+                        return object.name
+                    })
+console.log(filterOjects)
